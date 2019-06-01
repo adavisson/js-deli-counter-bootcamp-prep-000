04bb43adada969c63d9ea16fde3dcb4064b826c7
Andrew Davisson <akdavisson4@gmail.com>
2019-06-01 15:50:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function takeANumber(line, name){
   line.push(name);
   return `Welcome, ${name}. You are number ${line.length} in line.`;
 }
+
+function nowServing(line){
+  if(line){
+    return line.shift();
+  }else{
+    return 'There is nobody waiting to be served!';
+  }
+}
