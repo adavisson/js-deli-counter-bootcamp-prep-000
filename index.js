548151e68933882a0ecd0b9c59c06fa38b4e5475
Andrew Davisson <akdavisson4@gmail.com>
2019-06-01 15:58:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function currentLine(line){
     return 'The line is currently empty.';
   }else{
     var str = 'The line is currently: ';
-    for(let i = 1; i <= line.length; i++){
+    for(let i = 0; i < line.length; i++){
       str += `${i}. ${line.shift()}, `;
     }
     return str;
